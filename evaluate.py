d632dd4acc0d1f20f2470a16d480aa83a9689962
Lorenzo <lorenzo.tonet.03@gmail.com>
2026-09-14 18:30:31 +0200

    script for training with ppo, and see the model running

---
+++ evaluate.py
@@ -0,0 +1,27 @@
+import yaml
+import numpy as np
+from source_code.environment import SailingEnv, FlattenSailingObs
+from source_code.map_elements import Checkpoint
+from source_code.PPO import PPOAgent
+
+with open("./config.yaml") as f:
+    cfg = yaml.safe_load(f)
+
+cp1 = Checkpoint(np.array([20.0, 20.0]), radius=5.0, number=1)
+goal = Checkpoint(np.array([40.0, 20.0]), radius=5.0, number=2)
+checkpoints = [cp1, goal]
+
+env = SailingEnv(cfg, checkpoints=checkpoints, render_mode="human")
+env = FlattenSailingObs(env)
+
+agent = PPOAgent(state_dim=13, action_dim=1)
+agent.load("checkpoints/ppo_sailing.pt")
+
+state, _ = env.reset()
+done = truncated = False
+while not (done or truncated):
+    action, _ = agent.get_action(state)
+    state, reward, done, truncated, info = env.step(action)
+    env.render()
+
+env.close()
---
+++ train.py
@@ -0,0 +1,50 @@
+import argparse
+import yaml
+import numpy as np
+
+import pygame
+
+from source_code.environment import SailingEnv, FlattenSailingObs
+from source_code.vector_field import VecField
+from source_code.map_elements import Checkpoint
+from source_code.PPO import train_ppo_agent, PPOAgent
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default='./config.yaml', help="Path to config file")
+    args = parser.parse_args()
+
+    try:
+        with open(args.config, 'r') as f:
+            cfg = yaml.safe_load(f)
+    except FileNotFoundError:
+        print(f"Config file {args.config} not found. Exiting.")
+        raise SystemExit(1)
+
+    cp1 = Checkpoint(np.array([20.0, 20.0]), radius=5.0, number=1)
+    goal = Checkpoint(np.array([40.0, 20.0]), radius=5.0, number=2)
+    checkpoints = [cp1, goal]
+
+    env = SailingEnv(cfg, checkpoints=checkpoints, render_mode="human")
+    env = FlattenSailingObs(env)
+
+    observation, info = env.reset()
+
+    print("Initializing Proximal Policy Optimization (PPO-Clip) Agent...")
+    ppo_agent = PPOAgent(
+        state_dim=13,
+        action_dim=1,
+        lr=2e-3,
+        clip_ratio=0.2,
+        epochs=4
+    )
+    
+    # Train for fewer episodes because PPO converges much faster than basic Actor-Critic
+    n_episodes = 100
+    returns_ppo = train_ppo_agent(ppo_agent, env, n_episodes=n_episodes, update_timestep=1000)
+
+    ppo_agent.save("checkpoints/ppo_sailing.pt")
+
+    env.close()
+
