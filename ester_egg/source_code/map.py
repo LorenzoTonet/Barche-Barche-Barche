262b4ad1300d61136e373a6f85641d4532821076
Lorenzo Tonet <115221097+LorenzoTonet@users.noreply.github.com>
2026-09-23 14:04:47 +0200

    Delete easter_egg directory

--- ester_egg/source_code/map.py
+++
@@ -1,7 +0,0 @@
-import pygame as pg
-
-class Goal():
-    def __init__(self, x, y, width, height, image_path):
-        self.rect = pg.Rect(x, y, width, height)
-        self.image = pg.image.load(image_path).convert_alpha()
-
